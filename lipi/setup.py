8b25c4ab5411f38d9f8f5bf60cb5346902860aa3
rohit <rohitcbscient@gmail.com>
2019-01-30 12:11:02 +0100

    adding scripts

---
+++ lipi/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='surya', 
+        version='0.1',
+        description='Solar analysis libraries',
+        url='http://github.com/rohitcbscient/',
+        author='Rohit Sharma',
+        author_email='rohitcbscient@gmail.com',
+        license='',
+        packages=['surya'],
+        install_requires=['markdown',],
+        zip_safe=False)
