7483868692a6d076a1e295012fc00402fb2050b0
rohit <rohitcbscient@gmail.com>
2019-08-14 10:57:58 +0200

    20130423 event selfcal script

--- lipi/adhyan/vla/selfcal_20130423.py
+++ lipi/adhyan/vla/selfcal_20130423.py
@@ -4,12 +4,13 @@ import sys
 import numpy as np
 import os
 
-MS='/nas08-data02/rohit/20120225_selfcal_yingie/2050.1s.cal.ms' 
+MS='/nas08-data02/vladata/20130423/L-Band/selfcal/20130423T2030-2050.L.50ms.selfcal.ms' 
 split_=1
 if(split_==1):
-    OUTPUT_MS='2050.1s.cal.fl.ms'
-    os.system('rm -rf 2050.1s.cal.fl.m*')
-    timerange_='20:47:09~20:47:10'
+    OUTPUT_MS='20130423T203430-203431.L.50ms.ms'
+    os.system('rm -rf 20130423T203430-203431.L.50ms.m*')
+    #timerange_='20:45:00~20:45:30'
+    timerange_='20:34:30~20:34:31' # MARINA timestamps
     #timerange_='20:46:09~20:46:10'
     datacolumn_='data'
     print 'Splitting the MS....'+MS
@@ -19,23 +20,36 @@ if(split_==1):
             datacolumn=datacolumn_,keepflags=True,width=1, \
             timebin="0s",combine="")
     print 'Splitting Finished..'+MS
-    
-MS='2050.1s.cal.fl.ms'
+   
+
+
+# INPUT PARAMETERS
+MS='20130423T203430-203431.L.50ms.ms'
 prefix='selfsol/fsun.selfcal_soln'
 spw_='7'
-
 interactive=False
-modes=['ap','ap','ap','ap','ap']
+modes=['p','p','p','p']
+niter_list=[400,400,400,400]
+# RA:02 05 40.74 DEC:+12 44 01.6 20:34:00
+#phase_centre='J2000 0.548505138896rad 0.222291421955rad'
+phase_centre='J2000 02h05m40.74 +12d44m01.6'
 #modes=['ap']*1+['p']*2+['ap']
+
+# SCRIPTS STARTS
 std=[0]*len(modes)
 max_=[0]*len(modes)
 DR=[0]*len(modes)
 image_=[0]*len(modes)
+model_=[0]*(len(modes)+1)
 image_f=[0]*len(modes)
+model_[0]=[]
 for step in range(len(modes)):
     # Make model 
     image_[step]=prefix+'.spw.'+spw_+'.cal.'+str(step)+'.'+str(modes[step])
-    niter_=5
+    mod=image_[step]+'.model'
+    model_[step+1]=model_[step]+[mod]
+    print model_
+    niter_=niter_list[step]
     npercycle_=1
     make_model=1
     if(make_model):
@@ -49,7 +63,7 @@ for step in range(len(modes)):
             interpolation="linear",niter=niter_,gain=0.1,threshold="0.0mJy",psfmode="hogbom",imagermode="csclean", \
             ftmachine="mosaic",mosweight=False,scaletype="SAULT",multiscale=[],negcomponent=-1, \
             smallscalebias=0.6,interactive=True,mask=[],nchan=-1,start=0,width=1,outframe="",veltype="radio", \
-            imsize=[1024, 1024],cell=['2.5arcsec', '2.5arcsec'],phasecenter="",restfreq="", \
+            imsize=[1024, 1024],cell=['2.5arcsec', '2.5arcsec'],phasecenter=phase_centre,restfreq="", \
             stokes="I",weighting="natural",robust=0.0,uvtaper=False,outertaper=[''],innertaper=['1.0'], \
             modelimage="",restoringbeam=[''],pbcor=False,minpb=0.2,usescratch=False,noise="1.0Jy", \
             npixels=0,npercycle=npercycle_,cyclefactor=1.5,cyclespeedup=-1,nterms=1,reffreq="",chaniter=False, \
@@ -63,17 +77,17 @@ for step in range(len(modes)):
             interpolation="linear",niter=niter_,gain=0.1,threshold="0.0mJy",psfmode="hogbom",imagermode="csclean", \
             ftmachine="mosaic",mosweight=False,scaletype="SAULT",multiscale=[],negcomponent=-1, \
             smallscalebias=0.6,interactive=False,mask=[],nchan=-1,start=0,width=1,outframe="",veltype="radio", \
-            imsize=[1024, 1024],cell=['2.5arcsec', '2.5arcsec'],phasecenter="",restfreq="", \
+            imsize=[1024, 1024],cell=['2.5arcsec', '2.5arcsec'],phasecenter=phase_centre,restfreq="", \
             stokes="I",weighting="natural",robust=0.0,uvtaper=False,outertaper=[''],innertaper=['1.0'], \
-            modelimage="",restoringbeam=[''],pbcor=False,minpb=0.2,usescratch=False,noise="1.0Jy", \
+            modelimage='',restoringbeam=[''],pbcor=False,minpb=0.2,usescratch=False,noise="1.0Jy", \
             npixels=0,npercycle=npercycle_,cyclefactor=1.5,cyclespeedup=-1,nterms=1,reffreq="",chaniter=False, \
             flatnoise=True,allowchunk=False)
     # FT
     apply_model=1
     if(apply_model):
-        print 'Applying model...'+image_[step]+'.model'
+        print 'Applying model...'+str(model_[step+1])+'.model'
         #delmod(vis=MS)
-        ft(vis=MS,field="",spw=spw_,model=image_[step]+'.model',nterms=1,reffreq="",complist="",incremental=True,usescratch=True)
+        ft(vis=MS,field="",spw=spw_,model=model_[step+1],nterms=1,reffreq="",complist="",incremental=True,usescratch=True)
     # gaincal
     cal_gaincal=1
     if(cal_gaincal):
@@ -84,6 +98,19 @@ for step in range(len(modes)):
                 solint="inf",combine="",preavg=-1.0,refant="",refantmode="flex",minblperant=4,minsnr=3.0, \
                 solnorm=False,gaintype="G",smodel=[],calmode=modes[step],append=True,splinetime=3600.0, \
                 npointaver=3,phasewrap=180.0,docallib=False,callib="",gaintable=[],gainfield=[],interp=[],spwmap=[],parang=False)
+    # plotcal
+    plot_cal_amp=1
+    if(plot_cal_amp):
+        plotcal(caltable=caltable_,xaxis="antenna",yaxis="amp",poln="L",
+                field="",antenna="",spw="",timerange="",subplot=111,overplot=False,clearpanel="Auto",
+                iteration="",plotrange=[],showflags=False,plotsymbol="o",plotcolor="blue",markersize=5.0,
+                fontsize=10.0,showgui=False,figfile=caltable_+'_amp_ant.png')
+    plot_cal_phase=1
+    if(plot_cal_phase):
+        plotcal(caltable=caltable_,xaxis="antenna",yaxis="phase",poln="L",
+                field="",antenna="",spw="",timerange="",subplot=111,overplot=False,clearpanel="Auto",
+                iteration="",plotrange=[-1,-1,-180,180],showflags=False,plotsymbol="o",plotcolor="blue",markersize=5.0,
+                fontsize=10.0,showgui=False,figfile=caltable_+'_phase_ant.png')
     # applycal
     apply_cal=1
     if(apply_cal):
@@ -94,7 +121,7 @@ for step in range(len(modes)):
                 #gaintable=[caltable_], \
                 gaintable=image_[0:step+1], \
                 gainfield=[],interp=[], \
-                spwmap=[],calwt=[True],parang=False,applymode='calonly',flagbackup=True)
+                spwmap=[],calwt=[False],parang=False,applymode='calonly',flagbackup=True)
     make_image=1
     image_f[step]=image_[step]+'.final'
     if(make_image):
@@ -104,10 +131,10 @@ for step in range(len(modes)):
                 uvrange="",antenna="",scan="",observation="",intent="",mode="mfs",resmooth=False, \
                 gridmode="",wprojplanes=-1,facets=1,cfcache="cfcache.dir",rotpainc=5.0,painc=360.0, \
                 aterm=True,psterm=False,mterm=True,wbawp=False,conjbeams=True,epjtable="", \
-                interpolation="linear",niter=200,gain=0.1,threshold="0.0mJy",psfmode="hogbom",imagermode="csclean", \
+                interpolation="linear",niter=800,gain=0.1,threshold="0.0mJy",psfmode="hogbom",imagermode="csclean", \
                 ftmachine="mosaic",mosweight=False,scaletype="SAULT",multiscale=[],negcomponent=-1, \
                 smallscalebias=0.6,interactive=False,mask=[],nchan=-1,start=0,width=1,outframe="",veltype="radio", \
-                imsize=[1024, 1024],cell=['2.5arcsec', '2.5arcsec'],phasecenter="",restfreq="", \
+                imsize=[1024, 1024],cell=['2.5arcsec', '2.5arcsec'],phasecenter=phase_centre,restfreq="", \
                 stokes="I",weighting="natural",robust=0.0,uvtaper=False,outertaper=[''],innertaper=['1.0'], \
                 modelimage="",restoringbeam=[''],pbcor=False,minpb=0.2,usescratch=False,noise="1.0Jy", \
                 npixels=0,npercycle=10,cyclefactor=1.5,cyclespeedup=-1,nterms=1,reffreq="",chaniter=False, \
@@ -115,7 +142,10 @@ for step in range(len(modes)):
     img_=imstat(imagename=image_f[step]+".image/",axes=-1,region="",box="",chans="",stokes="",listit=True, \
             verbose=True,mask="",stretch=False,logfile="",append=True,algorithm="classic",fence=-1, \
             center="mean",lside=True,zscore=-1,maxiter=-1,clmethod="auto",niter=3)
-    std[step]=img_['rms']
+    img_std=imstat(imagename=image_f[step]+".image/",axes=-1,region="",box="10,10,100,100",chans="",stokes="",listit=True, \
+            verbose=True,mask="",stretch=False,logfile="",append=True,algorithm="classic",fence=-1, \
+            center="mean",lside=True,zscore=-1,maxiter=-1,clmethod="auto",niter=3)
+    std[step]=img_std['rms']
     max_[step]=img_['max']
     DR[step]=max_[step]/std[step]
     print DR[step],max_[step],std[step]
