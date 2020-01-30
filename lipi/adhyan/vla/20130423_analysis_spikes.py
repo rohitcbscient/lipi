5cf14d1c136fec9e10575ffe86a7591a37723f3b
rohit <rohitcbscient@gmail.com>
2020-01-30 08:28:25 +0100

    20200130_server

--- lipi/adhyan/vla/20130423_analysis_spikes.py
+++ lipi/adhyan/vla/20130423_analysis_spikes.py
@@ -50,8 +50,8 @@ freq=np.array(freq)
 data=np.array(data).swapaxes(0,1)
 time_sec=np.array(time_sec)
 max_=np.nanmax(data,axis=(2,3))
-xc_max1=np.array(xc_max)
-yc_max1=np.array(yc_max)
+xc_max1=np.array(xc_max).swapaxes(0,1)
+yc_max1=np.array(yc_max).swapaxes(0,1)
 datadump={'header':header,'data':data,'time':time,'time_sec':time_sec,'frequency':freq,'x_array':xc_array,'y_array':yc_array,'xc_max':xc_max1,'yc_max':yc_max1}
 pickle.dump(datadump,open('/nas08-data02/vladata/20130423/L-Band/sub_spikes/20130423T2030-2050.L.50ms.selfcal.sub.pol.LL.p','wb'))
 
@@ -59,7 +59,7 @@ sys.exit()
 
 import matplotlib.gridspec as gridspec
 from mpl_toolkits.axes_grid1 import make_axes_locatable
-nan_idx=np.where(max_<0.9e6)
+nan_idx=np.where(max_<2.0e6)
 max_[nan_idx]=np.nan
 xc_max1[nan_idx]=np.nan
 yc_max1[nan_idx]=np.nan
@@ -67,19 +67,19 @@ fig =plt.figure()
 spec = gridspec.GridSpec(ncols=2, nrows=2, figure=fig)
 ax1 = fig.add_subplot(spec[0, :])
 ax1.set_title('DS')
-im1=ax1.imshow(max_/1.e6,extent=[0,5,freq[0],freq[-1]],aspect='auto',interpolation=None,cmap='jet',origin='lower')
+im1=ax1.imshow(max_/1.e6,extent=[0,15,freq[0],freq[-1]],aspect='auto',interpolation=None,cmap='jet',origin='lower')
 divider = make_axes_locatable(ax1)
 cax = divider.append_axes('right', size='5%', pad=0.05)
 fig.colorbar(im1,cax=cax,orientation='vertical',label='MK')
 ax2 = fig.add_subplot(spec[1, 0],sharex=ax1,sharey=ax1)
 ax2.set_title('XC')
-im2=ax2.imshow(xc_max1,extent=[0,5,freq[0],freq[-1]],aspect='auto',interpolation=None,cmap='jet',origin='lower',vmin=620,vmax=690)
+im2=ax2.imshow(xc_max1,extent=[0,15,freq[0],freq[-1]],aspect='auto',interpolation=None,cmap='jet',origin='lower',vmin=620,vmax=690)
 divider = make_axes_locatable(ax2)
 cax = divider.append_axes('right', size='5%', pad=0.05)
 fig.colorbar(im2,cax=cax,orientation='vertical',label='arcsec')
 ax3 = fig.add_subplot(spec[1, 1],sharex=ax2,sharey=ax2)
 ax3.set_title('YC')
-im3=ax3.imshow(yc_max1,extent=[0,5,freq[0],freq[-1]],aspect='auto',interpolation=None,cmap='jet',origin='lower',vmin=240,vmax=260)
+im3=ax3.imshow(yc_max1,extent=[0,15,freq[0],freq[-1]],aspect='auto',interpolation=None,cmap='jet',origin='lower',vmin=240,vmax=260)
 divider = make_axes_locatable(ax3)
 cax = divider.append_axes('right', size='5%', pad=0.05)
 fig.colorbar(im3,cax=cax,orientation='vertical',label='arcsec')
