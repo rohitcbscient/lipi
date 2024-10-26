4c76e8326d40910fc2cf03acf44e950c1929132b
rohit <rohitcbscient@gmail.com>
2024-10-26 07:59:14 +0530

    working version of solar sim on ra

---
+++ lipi/adhyan/ska/karabo_simutils.py
@@ -0,0 +1,237 @@
+import numpy as np
+from astropy.io import fits
+from karabo.imaging.imager_oskar import OskarDirtyImager, OskarDirtyImagerConfig
+from karabo.imaging import imager_wsclean
+
+def get_end_freq(start_freq,bandwidth,samples):
+    """Get end frequency of the radio band
+    Caution:Give freq and bandwidth arguments in the same units
+    Args:
+        start_freq (float): Start Frequency
+        bandwidth (float): End Frequency
+        samples (int): number of samples
+    """
+    end_freq = start_freq+bandwidth
+    freq_arr = np.linspace(start_freq,end_freq,samples)
+    return end_freq,freq_arr
+
+def get_telescope(telpath):
+    """Get the SKA and it's precursor configurations
+
+    Paramaters:
+        telpath (str): Path to the telescope repo
+
+    Returns:
+        telescope_ska_combined: Paths for SKA-mid and SKA-low
+        telescope_precursor_combined: Paths for SKA precursor 
+    """
+    telescope_meerkat = telpath+'meerkat.tm'
+    telescope_mwaphase1 = telpath+'mwa.phase1.tm'
+    telescope_ska1mid = telpath+'ska1mid.tm'
+    telescope_ska1midAAs = telpath+'ska-mid-AAstar.tm'
+    telescope_ska1midAA2 = telpath+'ska-mid-AA2.tm'
+    telescope_ska1midAA1 = telpath+'ska-mid-AA1.tm'
+    telescope_ska1midAA05 = telpath+'ska-mid-AA0.5.tm'
+    telescope_ska1low = telpath+'ska1low.tm'
+    telescope_ska1lowAAs = telpath+'ska-low-AAstar.tm'
+    telescope_ska1lowAA2 = telpath+'ska-low-AA2.tm'
+    telescope_ska1lowAA1 = telpath+'ska-low-AA1.tm'
+    telescope_ska1lowAA05 = telpath+'ska-low-AA0.5.tm'
+    telescope_ska_combined = {'skamid':[telescope_ska1mid,telescope_ska1midAAs,telescope_ska1midAA2,\
+                            telescope_ska1midAA1,telescope_ska1midAA05],
+                            'skalow':[telescope_ska1low,telescope_ska1lowAAs,telescope_ska1lowAA2,\
+                            telescope_ska1lowAA1,telescope_ska1lowAA05]
+                            }
+    telescope_precursor_combined = {'mwa':[telescope_mwaphase1],'meerkat':[telescope_meerkat]}
+    return telescope_ska_combined,telescope_precursor_combined
+
+
+def get_telescope_config(telescope,info):
+    """Function gives the relavent information about the telescope config
+    Parameters
+    ----------
+        'telescope_name'(str): Path of the .tm file
+    Returns
+    -------
+    `base_length` (array): Array of baselines
+    `maxb` (float): Maximum baseline
+    `medb` (float): Median baseline
+    """
+    layout=np.loadtxt(telescope+'/layout.txt')
+    nant=len(layout)
+    nb=int(nant*(nant-1)*0.5)
+    base_length=[0]*nb
+    k=0
+    for i in range(nant):
+        for j in range(i,nant):
+            if(i!=j):
+                base_length[k] = np.sqrt((layout[i][0]-layout[j][0])**2 + (layout[i][1]-layout[j][1])**2)
+                k=k+1
+    base_length=np.array(base_length); maxb = np.nanmax(base_length); medb = np.nanmedian(base_length)
+    if(info):
+        print('---------------------')
+        print(telescope)
+        print("Total Baselines: ",nb)
+        print('Maximum Baseline (m):',np.round(maxb,2),'| Median Baseline (m):',np.round(medb,2))
+    return base_length,maxb,medb
+
+def ska_frequency_params(bandwidth_samples,info=None):
+    """SKA Parameters 
+    Note that all values are in MHz
+    Parameters
+    ----------
+        `baseline_samples`: Number of samples for dividing the bandwidth
+    Return
+    ------
+        ``
+        ska1low,ska1mid
+    """
+    start_freq_ska1_low = 50
+    start_freq_ska1_mid1 = 350
+    start_freq_ska1_mid2 = 950
+    start_freq_ska1_mid3 = 1650
+    start_freq_ska1_mid4 = 2800
+    start_freq_ska1_mid5a = 4600
+    start_freq_ska1_mid5b = 8300
+    #---------------------------
+    bandwidth_ska1_low = 300
+    bandwidth_ska1_mid1 = 700
+    bandwidth_ska1_mid2 = 810
+    bandwidth_ska1_mid3 = 1400
+    bandwidth_ska1_mid4 = 2380
+    bandwidth_ska1_mid5a = 3900
+    bandwidth_ska1_mid5b = 5000
+    #--------------------------
+    channel_width_ska1_low = 5.4e-3 
+    channel_width_ska1_mid1 = 5.4e-3
+    channel_width_ska1_mid2 = 13.4e-3
+    channel_width_ska1_mid3 = 13.4e-3
+    channel_width_ska1_mid4 = 13.4e-3
+    channel_width_ska1_mid5a = 80.6e-3
+    channel_width_ska1_mid5b = 80.4e-3
+    #---------------------------------
+    end_freq_ska1_low, freq_ska1_low = get_end_freq(start_freq_ska1_low,bandwidth_ska1_low,bandwidth_samples)
+    end_freq_ska1_mid1, freq_ska1_mid1 = get_end_freq(start_freq_ska1_mid1,bandwidth_ska1_mid1,bandwidth_samples)
+    end_freq_ska1_mid2, freq_ska1_mid2 = get_end_freq(start_freq_ska1_mid2,bandwidth_ska1_mid2,bandwidth_samples)
+    end_freq_ska1_mid3, freq_ska1_mid3 = get_end_freq(start_freq_ska1_mid3,bandwidth_ska1_mid3,bandwidth_samples)
+    end_freq_ska1_mid4, freq_ska1_mid4 = get_end_freq(start_freq_ska1_mid4,bandwidth_ska1_mid4,bandwidth_samples)
+    end_freq_ska1_mid5a, freq_ska1_mid5a = get_end_freq(start_freq_ska1_mid5a,bandwidth_ska1_mid5a,bandwidth_samples)
+    end_freq_ska1_mid5b, freq_ska1_mid5b = get_end_freq(start_freq_ska1_mid5b,bandwidth_ska1_mid5b,bandwidth_samples)
+    #----------------------------------------------------------------------------------------------------------------
+    tellow_param = [[freq_ska1_low,start_freq_ska1_low,bandwidth_ska1_low,end_freq_ska1_low,channel_width_ska1_low]]
+    freq_ska1_mid = np.vstack((freq_ska1_mid1,freq_ska1_mid2,freq_ska1_mid3,freq_ska1_mid4,freq_ska1_mid5a,freq_ska1_mid5b))
+    start_freq_ska1_mid = np.vstack((start_freq_ska1_mid1,start_freq_ska1_mid2,start_freq_ska1_mid3,start_freq_ska1_mid4,start_freq_ska1_mid5a,start_freq_ska1_mid5b))
+    end_freq_ska1_mid = np.vstack((end_freq_ska1_mid1,end_freq_ska1_mid2,end_freq_ska1_mid3,end_freq_ska1_mid4,end_freq_ska1_mid5a,end_freq_ska1_mid5b))
+    bandwidth_ska1_mid = np.vstack((bandwidth_ska1_mid1,bandwidth_ska1_mid2,bandwidth_ska1_mid3,bandwidth_ska1_mid4,bandwidth_ska1_mid5a,bandwidth_ska1_mid5b))
+    channel_width_ska1_mid = np.vstack((channel_width_ska1_mid1,channel_width_ska1_mid2,channel_width_ska1_mid3,channel_width_ska1_mid4,channel_width_ska1_mid5a,channel_width_ska1_mid5b))
+    telmid_param = [freq_ska1_mid,start_freq_ska1_mid,bandwidth_ska1_mid,end_freq_ska1_mid,channel_width_ska1_mid]
+    tel_params = {'low':tellow_param,'mid':telmid_param}
+    if(info):
+        print('---- Array Information (MHz) ---------')
+        print('(1) frequency array, (2) start frequency, '\
+            '(3) bandwidth, (4) end frequency, (5) uniform channel width')
+    return tel_params
+
+
+def get_skydata(skymodel_path,sm_save_str,start_frequency_hz_,ra_sun_center,dec_sun_center,\
+                skymodel_cellsize,sm_fscale,sm_threshold, save_sources,source_cut,\
+                add_sm, randsize, point_flux, flux_rand_low, flux_rand_high,angle_rand):
+    """Get skymodel data with optional features of adding point sources
+
+    Parameters:
+        skymodel_path (str): path of the skymodel
+        sm_save_str (str): path to save skymodel
+        start_frequency_hz_ (float): start frequency in Hz
+        ra_sun_center (float): ra of the center in degree
+        dec_sun_center (float): dec of the center in degree
+        sm_fscale (float): spectral index to scale flux density with frequency
+        sm_threshold (float): cut in flux density of skymodel w.r.t maximum flux density
+        save_sources (bool): save skymodel added sources (not entire skymodel)
+        source_cut (int): cut on the number of source
+        randsize (int): size of randomly (uniform) added sources
+        point_flux (float): flux density of point source
+        flux_rand_low (float): lower end of the random source distribution
+        flux_rand_high (float): higher end of the random source distribution
+        angle_rand (float): angular extent of the random source distribution w.r.t solar center 
+
+    Returns:
+        `sky_array` (array): Array of the skymodel (disk + added sources)
+        `solar_map` (image): 2-D array of the disk skymodel at reference frequency (240 MHz)
+        `solar_map_jy` (image): Frequency scaled flux density map in Jansky
+    """
+    solar_map,smh=fits.getdata(skymodel_path,header=True)
+    solar_map_jy=solar_map/np.nanmax(solar_map)\
+                    *20*1.e4*(start_frequency_hz_/2.4e8)**sm_fscale
+    ra_grid,dec_grid=np.meshgrid((np.arange(smh['NAXIS1'])-smh['NAXIS1']/2)*skymodel_cellsize/3600.,
+                                (np.arange(smh['NAXIS2'])-smh['NAXIS2']/2)*skymodel_cellsize/3600.)
+    ra_grid=ra_grid+ra_sun_center;dec_grid=dec_grid+dec_sun_center
+    idx=np.where(solar_map>sm_threshold*np.nanmax(solar_map))
+    sky_model_ra=ra_grid[idx];sky_model_dec=dec_grid[idx];flux=solar_map_jy[idx]
+    sky_data = np.array([sky_model_ra, sky_model_dec, flux,np.zeros(len(flux)), \
+            np.zeros(len(flux)),np.zeros(len(flux)), np.zeros(len(flux)),np.zeros(len(flux)), \
+        np.zeros(len(flux)),np.zeros(len(flux)), np.zeros(len(flux)),np.zeros(len(flux))]).T
+    sky_data=sky_data[0:source_cut,:]
+    if(add_sm=='point'):
+        size_=1
+        add_source = np.zeros((size_,12))
+        add_source[0][0] = ra_sun_center # RA
+        add_source[0][1] = dec_sun_center # DEC
+        add_source[0][2] = point_flux # Flux
+        sky_data1 = np.vstack((sky_data,add_source))
+    elif(add_sm=='random'):
+        size_=randsize
+        add_source = np.zeros((size_,12))
+        ra_point_array = np.random.uniform(low=ra_sun_center-angle_rand, high=ra_sun_center+angle_rand, size=size_)
+        dec_point_array = np.random.uniform(low=dec_sun_center-angle_rand, high=dec_sun_center+angle_rand, size=size_)
+        flux_array = np.random.uniform(low=flux_rand_low, high=flux_rand_high, size=size_)
+        add_source[:,0] = ra_point_array # RA
+        add_source[:,1] = dec_point_array # DEC
+        add_source[:,2] = flux_array # Flux
+        sky_data1 = np.vstack((sky_data,add_source))
+    else:
+        sky_data1 = sky_data
+    if(save_sources):
+        np.savetxt(sm_save_str+'.sm', add_source.T)
+    return sky_data1,solar_map,solar_map_jy
+
+def make_images(vis,imager_str,imgsize,img_str,cellsize_rad,niter,weight,maxuv,minuv,nchan,vis_str):
+    """Make either `oskar` dirty or `wsclean` images
+
+    Parameters:
+        vis (object): visibility instance
+        imager_str (_type_): _description_
+        imgsize (_type_): _description_
+        img_str (_type_): _description_
+        cellsize_rad (_type_): _description_
+        niter (_type_): _description_
+        weight (_type_): _description_
+        maxuv (_type_): _description_
+        minuv (_type_): _description_
+        nchan (_type_): _description_
+        vis_str (_type_): _description_
+
+    Returns:
+        image (image_instance): _description_
+    """
+    dirty_image='no_image';restored='no_image'
+    if(imager_str=='oskar'):
+        dirty_imager = OskarDirtyImager(
+        OskarDirtyImagerConfig(
+            imaging_npixel=imgsize,
+            imaging_cellsize=cellsize_rad,
+        ))
+        dirty_image = dirty_imager.create_dirty_image(vis,output_fits_path=img_str+'oskar.fits')
+    if(imager_str=='wsclean'):
+        img_cmd = 'wsclean \
+                -size '+str(imgsize)+' '+str(imgsize)+' \
+                -name '+img_str+' \
+                -scale '+str(cellsize_rad)+'rad -niter '+str(niter)+' -mgain 0.8 \
+                -weight '+weight+'\
+                -maxuv-l '+str(maxuv)+' -minuv-l '+str(minuv)+'\
+                -channels-out '+str(nchan)+' '+vis_str+'.ms'
+        print(img_cmd)
+        try:
+            restored = imager_wsclean.create_image_custom_command(command=img_cmd)
+        except:
+            pass # doing nothing on exception
+    return restored,dirty_image
--- lipi/adhyan/ska/solar_karabo_ra_script.py
+++ lipi/adhyan/ska/solar_karabo_ra_script.py
@@ -16,113 +16,153 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 #mpl.use('Tkagg')
 from IPython.core.magic import register_cell_magic
+import sys
+from karabo_simutils import *
 @register_cell_magic
 def skip(line, cell):
     return
 
 
-#telescope_path = '/data/rohit/skao_repo/meerkat.tm'
-#telescope_path = '/data/rohit/skao_repo/mwa.phase1.tm'
-#telescope_path = '/data/rohit/skao_repo/ska1mid.tm'
-#telescope_path = '/data/rohit/skao_repo/ska1low.tm'
-#telescope_path = '/data/rohit/skao_repo/ska-mid-AAstar.tm'
-#telescope_path = '/data/rohit/skao_repo/ska-mid-AA2.tm'
-#telescope_path = '/data/rohit/skao_repo/ska-mid-AA1.tm'
-#telescope_path = '/data/rohit/skao_repo/ska-low-AAstar.tm'
-#telescope_path = '/data/rohit/skao_repo/ska-low-AA2.tm'
-#telescope_path = '/data/rohit/skao_repo/ska-low-AA1.tm'
-#telescope_path = '/media/rohit/sdata/ska-solar-files/mwa.phase1.tm'
-#telescope_path = '/media/rohit/sdata/ska-solar-files/ska1low.tm'
-#telescope_name='skamid_AA2_full_snap0_100MHz-2-2000GHz'
-telescope_name='skalow_full_point_one_snap0_100MHz-2-2000GHz'
-#telescope_name='meerkat_point_rand_snap0_100MHz-2-2000GHz'
-#telescope_name='mwa_full_snap0_100MHz-2-2000GHz'
-#dtime=datetime(2000, 1, 1, 10, 0, 00, 0) # MeerKAT/ SKA-mid
-dtime=datetime(2000, 1, 1, 4, 0, 00, 0) # MWA / SKA-low
+
+#----- Define your Telescopes for the Simulation
+skao_repo_tel_path = '/data/rohit/skao_repo/'
+tel_all = get_telescope(skao_repo_tel_path) # 0 is SKA, and 1 is SKA-precursor
+telescope_list = ['skalow','skamid','mwa','meerkat']
+ska_aa_list = ['AA0.5','AA1','AA2','AAs','Full']
+mid_bands=['1','3']
+nt=0; tel = telescope_list[nt] # Put in your telescope in `tel`
+
+#----- Define your Frequency Array for the Simulation
+freq_samples = 10
+ska_freq_param = ska_frequency_params(bandwidth_samples=freq_samples)
+if(tel[0:3]=='ska' ):
+    if(tel=='skamid' or tel=='meerkat'):
+        bands=mid_bands
+        midbands_list=['1','2','3','4','5a','5b']
+        freq_array=[0]*len(mid_bands);i=0
+        for m in mid_bands:
+            bandsidx_ = midbands_list.index(m)
+            freq_array[i] = ska_freq_param['mid'][0][bandsidx_]
+            i+=1
+        freq_array = np.array(freq_array).flatten()
+    else:
+        freq_array = ska_freq_param['low'][0][0]
+    
+#----- Define the Observations
+if(tel=='skamid' or tel=='meerkat'):
+    dtime=datetime(2000, 1, 1, 10, 0, 00, 0) # MeerKAT/ SKA-mid
+if(tel=='skalow' or tel=='mwa'):
+    dtime=datetime(2000, 1, 1, 4, 0, 00, 0) # MWA / SKA-low
 hour_=0
 minutes_=1
 noise_enable_ = False
 enable_array_beam=False
+
+#-------- Define SkyModel Parameters
+skymodel_path='/data/rohit/20151203_240MHz_psimas.fits'
+ra_sun_center=249.141666667;dec_sun_center=21.986 #16 34 34.52 -21 59 09.7
+skymodel_cellsize=22.5 # in arcsec
+sm_threshold = 0.1
+sm_fscale = 1
+add_sm = 'random' # 'point' / 'random'
 point_source_bool = False # tag 1 / tag 0 is ideal case
+point_flux = 100.e4
 random_points = False # tag 2
-nchan = 1
-ntchan = 1
-if(random_points==True):
+if(add_sm=='random'):
     size_ = 100
 else:
     size_ = 1
-point_flux = 100.e4
+randsize = 100
+flux_rand_low = 10 # Jy
+flux_rand_high = 100 # Jy
+angle_rand = 0.3 # in degrees
+source_cut=16000
+save_sources = True
 
-ran = range(18)
+#-------- Define Image Parameters
+#npix_per_beam = 3
+imgsize=4096
+cellsize_arcsec=1 #beam_size_arcsec/npix_per_beam
+cellsize_rad=cellsize_arcsec/3600*np.pi/180 # in rad
+imager_str = 'wsclean'
+niter = 1
+maxuv = 9000 # in lambda units
+minuv = 10 # in lambda units
+nchan = 1
+ntchan = 1
+weight = 'uniform'
+    
+#for naa in range(len(ska_aa_list)): # loop over array assemblies 
+for naa in range(1): # loop over array assemblies 
+    telescope_path = tel_all[0][telescope_list[nt]][naa] # 0 --> length of 4 where 0 is SKA-full and 4 is AA0.5
+    bl_array,maxbl,medbl=get_telescope_config(telescope=telescope_path,info=True) # bl -> Baseline
+    
+    #for j in range(len(freq_array)): # Loop over the frequencies
+    for j in range(1): # Loop over the frequencies
+        start_frequency_hz_ = freq_array[j]*1.e6
+        #----------- Define strings and names
+        path_ = '/data/rohit/ska-solar-sim-repo/'+tel+'/'
+        sm_type = 'rand'
+        tel_str = telescope_path.split('repo/')[1].split('.t')[0]
+        sm_str = 'forward-20151203-'+sm_type
+        freq_str = 'freq-'+str(np.round(freq_array[j],2))
+        obs_str = 'snap-1-min-1-channel'
+        img_str = 'img-cellsize-'+str(cellsize_arcsec)+'-imsize-'+str(imgsize)
+        vis_str = path_+'solar_'+tel_str+'_'+sm_str+'_'+freq_str+'_'+obs_str+'_sim'
+        sm_save_str = path_+'solar_'+tel_str+'_'+sm_str+'_'+freq_str+'_'+obs_str+'_skymodel'
+        img_str = vis_str+'_'+img_str
+        
+        #------------- Get Skymodel
+        sky_data1,solar_map,solar_map_jy=get_skydata(skymodel_path,sm_save_str, start_frequency_hz_,\
+            ra_sun_center,dec_sun_center, skymodel_cellsize,sm_fscale,\
+            sm_threshold, save_sources,source_cut,\
+            add_sm, randsize, point_flux, flux_rand_low, flux_rand_high,angle_rand)
+        
+        #------------ Simulation starts
+        sky = SkyModel()
+        sky.add_point_sources(sky_data1)
+        backend=SimulatorBackend.OSKAR
+        telescope=Telescope.read_OSKAR_tm_file(telescope_path)
+        telescope.read_OSKAR_tm_file(telescope_path)
+        simulation = InterferometerSimulation(vis_path=vis_str+'.vis', ms_file_path=vis_str+'.ms',
+            channel_bandwidth_hz=1, time_average_sec=10, noise_enable=noise_enable_, use_gpus=True,
+            noise_seed="time", noise_freq="Range", noise_rms="Range",
+            noise_start_freq=1.e9, noise_inc_freq=1.e8, noise_number_freq=24,
+            noise_rms_start=0, noise_rms_end=0, enable_numerical_beam=enable_array_beam,
+            enable_array_beam=enable_array_beam)
+        observation = Observation(mode='Tracking',phase_centre_ra_deg=ra_sun_center, start_date_and_time=dtime,
+            length=timedelta(hours=hour_, minutes=minutes_, seconds=0, milliseconds=0),
+            phase_centre_dec_deg=dec_sun_center, number_of_time_steps=ntchan,
+            start_frequency_hz=start_frequency_hz_, frequency_increment_hz=1,
+            number_of_channels=nchan, )
+        visibility = simulation.run_simulation(telescope, sky, observation, backend=backend)
+                
+        #--------- Get the images
+        image=make_images(visibility,imager_str,imgsize,img_str,cellsize_rad,\
+                        niter,weight,maxuv,minuv,nchan,vis_str)
 
 
-layout=np.loadtxt(telescope_path+'/layout.txt')
-nant=len(layout)
-nb=int(nant*(nant-1)*0.5)
-print('Number of Baselines:',nb)
-base_length=[0]*nb
-k=0
-for i in range(nant):
-    for j in range(i,nant):
-        if(i!=j):
-            base_length[k] = np.sqrt((layout[i][0]-layout[j][0])**2 + (layout[i][1]-layout[j][1])**2)
-            k=k+1
-base_length=np.array(base_length)
-print('Maximum Baseline',base_length.max())
+sys.exit()
+
+
+
 #----------------------------
-freq_list = np.arange(1,20)*100 # in MHz
-beamsize_arr = 3.e8/base_length.max()/(freq_list*1.e6)
-beamsize_arr_arcsec = beamsize_arr*180/np.pi*3600
-f,ax=plt.subplots(1,1)
-ax.plot(freq_list,beamsize_arr_arcsec,'o-')
-ax.set_xlabel('Frequency (MHz)')
-ax.set_ylabel('Max. Resolution (arcsec)')
-ax.set_title('SKA-mid')
-plt.close()
+#freq_list = freq_array
+#beamsize_arr = 3.e8/base_length.max()/(freq_list*1.e6)
+#beamsize_arr_arcsec = beamsize_arr*180/np.pi*3600
+#f,ax=plt.subplots(1,1)
+#ax.plot(freq_list,beamsize_arr_arcsec,'o-')
+#ax.set_xlabel('Frequency (MHz)')
+#ax.set_ylabel('Max. Resolution (arcsec)')
+#ax.set_title('SKA-mid')
+#plt.close()
 
 
-path = '/data/rohit/ska-low-sim/'
 for i in ran:
     start_frequency_hz_ = freq_list[i]*1.e6
     beam_size_arcsec = 3.e8/start_frequency_hz_/base_length.max()*180/np.pi*3600
     print('Maximum Baseline',base_length.max(), 'Beam (arcsec)',beam_size_arcsec)
     print("Frequency (MHz): ",start_frequency_hz_)
-    aa=fits.open('/data/rohit/20151203_240MHz_psimas.fits')
-    ms_filename = path+"solar_"+"freq_"+str(int(freq_list[i]))+'_'+telescope_name+".ms"
-    vis_filename = path+"solar_"+"freq_"+str(int(freq_list[i]))+'_'+telescope_name+".vis"
-    solar_map=aa[0].data;solar_map_jy=solar_map/np.nanmax(solar_map)*20*1.e4*(start_frequency_hz_/2.4e8)**1
-    ra_sun_center=249.141666667;dec_sun_center=21.986 #16 34 34.52 -21 59 09.7
-    ra_grid,dec_grid=np.meshgrid((np.arange(256)-128)*22.5/3600.,(np.arange(256)-128)*22.5/3600.)
-    ra_grid=ra_grid+ra_sun_center;dec_grid=dec_grid+dec_sun_center
-    idx=np.where(solar_map>0.1*np.nanmax(solar_map))
-    sky_model_ra=ra_grid[idx];sky_model_dec=dec_grid[idx];flux=solar_map_jy[idx]
-    # Simulation starts
-    sky = SkyModel()
-    sky_data = np.array([sky_model_ra, sky_model_dec, flux,np.zeros(len(flux)), \
-        np.zeros(len(flux)),np.zeros(len(flux)), np.zeros(len(flux)),np.zeros(len(flux)), \
-    np.zeros(len(flux)),np.zeros(len(flux)), np.zeros(len(flux)),np.zeros(len(flux))]).T
-    sky_data=sky_data[0:16000,:]
-    # Add Point Soure
-    add_source = np.zeros((size_,12))
-    if(point_source_bool):
-        add_source[0][0] = ra_sun_center # RA
-        add_source[0][1] = dec_sun_center # DEC
-        add_source[0][2] = point_flux # Flux
-        sky_data1 = np.vstack((sky_data,add_source))
-    elif(random_points):
-        ra_point_array = np.random.uniform(low=ra_sun_center-0.3, high=ra_sun_center+0.3, size=size_)
-        dec_point_array = np.random.uniform(low=dec_sun_center-0.3, high=dec_sun_center+0.3, size=size_)
-        flux_array = np.random.uniform(low=1.e1, high=1.e2, size=size_)
-        add_source[:,0] = ra_point_array # RA
-        add_source[:,1] = dec_point_array # DEC
-        add_source[:,2] = flux_array # Flux
-        sky_data1 = np.vstack((sky_data,add_source))
-    else:
-        sky_data1 = sky_data
-    print('Maximum flux density (SFU):',sky_data1[:,2].max()/1.e4)
-    print('Number of Sources:',len(sky_data1))
-    sky.add_point_sources(sky_data1)
-    np.savetxt(path+"source_"+telescope_name+".txt", add_source.T)
     #telescope_name="SKA1MID"
     #telescope=Telescope.get_SKA1_LOW_Telescope()
     backend=SimulatorBackend.OSKAR
@@ -141,12 +181,7 @@ for i in ran:
         start_frequency_hz=start_frequency_hz_, frequency_increment_hz=1,
         number_of_channels=nchan, )
     visibility = simulation.run_simulation(telescope, sky, observation, backend=backend)
-    npix_per_beam = 3
-    imgsize=4096
-    cellsize_arcsec=1 #beam_size_arcsec/npix_per_beam
-    cellsize_rad=cellsize_arcsec/3600*np.pi/180 # in rad
-    print('Cellsize:',cellsize_arcsec,'Beam (arcsec):',beam_size_arcsec,'Max length:',base_length.max())
-    print('Field of View (deg):',imgsize*cellsize_arcsec/3600.)
+
     dirty_imager = OskarDirtyImager(
         OskarDirtyImagerConfig(
             imaging_npixel=imgsize,
@@ -167,6 +202,9 @@ for i in ran:
         restored = imager_wsclean.create_image_custom_command(command=img_cmd)
     except:
         pass # doing nothing on exception
+
+
+
 
 
 
